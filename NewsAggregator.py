cf4648822d208faa63d8724a1756101f757f3e37
Borys Róg <38949146+brsrg@users.noreply.github.com>
2020-08-13 21:53:18 +0200

    Add files via upload

---
+++ NewsAggregator.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Parse RSS feeds of the given websites to return latest posts.
+# The script will print out basic details of a post in a formatted
+# form and limit the results to 10 entries.
+
+import feedparser
+from itertools import islice
+
+Wired = feedparser.parse("https://www.wired.com/feed/rss")
+TechCrunch = feedparser.parse("http://feeds.feedburner.com/TechCrunch/")
+ComputerWorld = feedparser.parse("https://www.computerworld.com/news/index.rss")
+TechRadar = feedparser.parse("https://www.techradar.com/rss")
+Motherboard = feedparser.parse("https://www.vice.com/en_us/rss/section/tech")
+
+limit = 10
+
+
+def site(siteName, siteTitle):
+    print(siteTitle + "\n")
+    for post in islice((siteName.entries), limit):
+        print(
+            "------Date-------"
+            + "\n"
+            + post.published
+            + "\n"
+            + "------Title-------"
+            + "\n"
+            + post.title
+            + "\n"
+            + "------Link-------"
+            + "\n"
+            + post.link
+            + "\n"
+        )
+
+
+site(Wired, "Wired")
+site(TechCrunch, "TechCrunch")
+site(ComputerWorld, "Computer World")
+site(TechRadar, "TechRadar")
+site(Motherboard, "Vice Motherboard")
